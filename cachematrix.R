b71dc2322abbdd0b3d71cb4020ace678bf590690
placenameday <placenameday@gmail.com>
2022-01-09 18:18:02 +0800

    First assignment submission.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Determine whether the matrix is invertible, calculate 
+## the inverse matrix, and cache it.
 
-## Write a short comment describing this function
+## dm will judge the matrix passed into function A to determine whether
+## it is an invertible matrix or not.
+dm <- function(m) "matrix" %in% class(try(solve(m),silent=TRUE))
+
+## makeCacheMatrix is used to create a special vector containing 4 
+## functions and the environment in which its creation is contained.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  if(dm(x) == TRUE) {
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinver <- function(inver) m <<- inver
+    getinver <- function() m
+    list(set = set, get = get,
+         setinver = setinver,
+         getinver = getinver)
+  } else {
+    print ("The matrix is not invertible, please re-enter an invertible matrix!")
+  }
+  
 }
 
-
-## Write a short comment describing this function
+## cacheSolve is used to calculate the inverse matrix of the matrix created by
+## function makeCacheMatrix. It will first determine whether the inverse matrix
+## has been calculated, and if so, it will be directly retrieved from the
+## cache total, otherwise it will be calculated.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinver()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinver(m)
+  m
 }
